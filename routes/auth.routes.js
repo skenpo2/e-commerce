5248ceeca7f6d5ab4c342baf4475afc49cb6ff56
Micheal <michealobarewon2015@gmail.com>
2025-04-01 09:45:38 +0100

    login limiter

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
+const loginLimiter = require('../middlewares/loginLimiter');
+
 const {
   registerUser,
   loginUser,
@@ -10,7 +12,7 @@ const {
 } = require('../controllers/auth.controller');
 
 router.post('/register', registerUser);
-router.post('/login', loginUser);
+router.post('/login', loginLimiter, loginUser);
 router.post('/logout', logout);
 router.get('/refresh-token', refresh);
 
